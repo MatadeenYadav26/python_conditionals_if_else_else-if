2204958a3af57d7b763395f21acd4668432c0a71
Matadeen Yadav <matadeenyadav202@gmail.com>
2025-12-11 12:51:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-age = int(input('Enter Your Age: '))
+# age = int(input('Enter Your Age: '))
 
-if age >= 18 :
-    print('You are eligible to vote')
+# if age >= 18 :
+#     print('You are eligible to vote')
+# else:
+#     print('Un-eligible to vote')
+
+# #ternary operators
+
+# print("Vote") if age >= 18 else print("cannot vote!")
+
+#elif_statement
+
+money = int(input("Paise dedo: "))
+
+if money == 10 :
+    print("I will eat Mango Dolly")
+
+elif money == 20 :
+    print("I will eat Choclate Cake")
+
+elif money == 30:
+    print("I will eat Bobal Sprite")
+
 else:
-    print('Un-eligible to vote')
-
-#ternary operators
-
-print("Vote") if age >= 18 else print("cannot vote!")
+    print('I will eat XYZ.')
