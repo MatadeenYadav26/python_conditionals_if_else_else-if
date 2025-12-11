d2b1a69ca3bf5e08cbd34ce0333d08643cb40b12
Matadeen Yadav <matadeenyadav202@gmail.com>
2025-12-11 13:11:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,30 @@
 
 #elif_statement
 
-money = int(input("Paise dedo: "))
+# money = int(input("Paise dedo: "))
 
-if money == 10 :
-    print("I will eat Mango Dolly")
+# if money == 10 :
+#     print("I will eat Mango Dolly")
 
-elif money == 20 :
-    print("I will eat Choclate Cake")
+# elif money == 20 :
+#     print("I will eat Choclate Cake")
 
-elif money == 30:
-    print("I will eat Bobal Sprite")
+# elif money == 30:
+#     print("I will eat Bobal Sprite")
 
-else:
-    print('I will eat XYZ.')
+# else:
+#     print('I will eat XYZ.')
+
+
+#use of operators in conditionals
+
+a = 10
+b = 40
+c = 30
+
+if a > b and a > c :
+    print('a is the largest number.')
+elif b > a and b > c :
+    print('b is the largest number.')
+elif c > a and c > b :
+    print('c is the largest number.')
